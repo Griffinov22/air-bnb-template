8a7261a907d66fbb870f8a6c99f4dccde799f906
Griffinov22 <griffinovermyer@gmail.com>
2022-12-07 11:59:33 -0500

    setingup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// this file is tied to index.html
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
